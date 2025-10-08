891ff1e11bf2887ddf55d2f3b7b772c12007eeff
Tetsuo Fukumori <happy@happybell.jp>
2025-10-08 09:40:44 +0900

    ログイン情報が変更されました　をコメントアウトした
    ログイン中なのに勝手にログアウトされてしまうため、該当するソースをコメントアウトして、アラートが出ない様にした。

--- lib/pages/staff/staff_page.dart
+++ lib/pages/staff/staff_page.dart
@@ -184,29 +184,29 @@ class _StaffPageState extends State<StaffPage>
       return null;
     });
 
-    if (data == null) {
-      _stopCheckAccountTimer();
-
-      await showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('確認'),
-            content: Text('ログイン情報が更新されました。再度ログインをお願いします。'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-      _logout();
-    }
+    // if (data == null) {
+    //   _stopCheckAccountTimer();
+    //
+    //   await showDialog(
+    //     context: context,
+    //     barrierDismissible: false,
+    //     builder: (BuildContext context) {
+    //       return AlertDialog(
+    //         title: const Text('確認'),
+    //         content: Text('ログイン情報が更新されました。再度ログインをお願いします。'),
+    //         actions: <Widget>[
+    //           TextButton(
+    //             child: const Text('OK'),
+    //             onPressed: () {
+    //               Navigator.of(context).pop();
+    //             },
+    //           ),
+    //         ],
+    //       );
+    //     },
+    //   );
+    //   _logout();
+    // }
     }
 
   Future<void> _logout() async {
